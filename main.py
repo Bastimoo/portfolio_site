c1a018ad02f1b99e22a32ad72a02731a2dcfaaa1
Bastian Hörger <b.hoerger@web.de>
2023-08-15 13:06:52 +0200

    Added column, photo, basic text

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+import streamlit as st
+import csv
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+st.set_page_config(layout="wide")
 
+col1, col2 = st.columns(2)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+with col1:
+    st.image("images/photo.png", width=500)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with col2:
+    st.title("Bastian Hörger")
+    selfdescription = """
+    This is a placeholder for text about me!
+    """
+    st.info(selfdescription)
